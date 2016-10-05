4fe00909ef9aa06c2f1833ab6ffd194174c8d1b0
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-10-05 15:12:13 +0200

    Correcting the issue with jenkins build
    Given tests where testing a function that was deleted during
    the implementation of the MainActivity.

--- app/src/androidTest/java/ch/epfl/sweng/project/ExampleAndroidTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/ExampleAndroidTest.java
@@ -18,16 +18,6 @@ public final class ExampleAndroidTest {
     // TODO replace this with your own tests.
 
     @Test
-    public void onePlusOneIsTwo() {
-        assertThat(MainActivity.add(1, 1), is(2));
-    }
-
-    @Test
-    public void twoPlusTwoIsFour() {
-        assertThat(MainActivity.add(2, 2), is(4));
-    }
-
-    @Test
     public void packageNameIsCorrect() {
         final Context context = InstrumentationRegistry.getTargetContext();
         assertThat(context.getPackageName(), is("ch.epfl.sweng.project"));
