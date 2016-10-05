44d6f4bc710c683ec30b25f18e243633ae641391
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-10-05 13:00:59 +0200

    Sprint#1 implementation
    Create two activities, MainActivity, NewTaskActivity.
    We have now a listView and an add button which open an activity
    to create a new task.
    Deleting and adding the element to the listView is still
    not implemented.

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,21 +1,36 @@
 package ch.epfl.sweng.project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
-/**
- * Your app's main activity.
- */
 public final class MainActivity extends AppCompatActivity {
-    // TODO replace this code with your app code!
-
-    public static int add(final int a, final int b) {
-        return a + b;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.tasks_container, new TaskFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /* This method is called when the user click on the add button*/
+    public void openNewTaskActivity(View v) {
+        Intent intent = new Intent(this, NewTaskActivity.class);
+        startActivity(intent);
     }
 }
