a688e446ac639387f091cb6686e5b997232c4e31
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-10-06 14:48:07 +0200

    Spring#1 JavaDoc
    Add the JavaDoc for the MainActivity and Task classes.

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -7,24 +7,39 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
+/**
+ * MainActivity
+ */
 public final class MainActivity extends AppCompatActivity {
 
     private final int newTaskRequestCode = 1;
     private TaskFragment fragment;
 
+    /**
+     * Override the onCreate method to create a TaskFragment
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously
+     *                           being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragment = new TaskFragment();
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.tasks_container, fragment)
                     .commit();
         }
     }
 
+    /**
+     * Inflate the main_menu layout
+     *
+     * @param menu The options menu in which you place your items
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -32,12 +47,27 @@ public final class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    /* This method is called when the user click on the add button*/
+    /**
+     * Method called when add_task_button is clicked.
+     * It start a NewTaskActivity with startActivityForResult
+     *
+     * @param v Required argument
+     */
     public void openNewTaskActivity(View v) {
         Intent intent = new Intent(this, NewTaskActivity.class);
         startActivityForResult(intent, newTaskRequestCode);
     }
 
+    /**
+     * Method called when an activity launch inside MainActivity,
+     * is finished. This method is triggered only if we use
+     * startActivityForResult.
+     *
+     * @param requestCode The integer request code supplied to startActivityForResult
+     *                    used as an identifier.
+     * @param resultCode  The integer result code returned by the child activity
+     * @param data        An intent which can return result data to the caller.
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == newTaskRequestCode) {
--- app/src/main/java/ch/epfl/sweng/project/Task.java
+++ app/src/main/java/ch/epfl/sweng/project/Task.java
@@ -6,15 +6,40 @@ import android.os.Parcelable;
 /**
  * Task is the class representing a task
  */
-public class Task implements Parcelable {
+class Task implements Parcelable {
+    /**
+     * Used to regenerate a Task, all parcelables must have a creator
+     */
+    public static final Parcelable.Creator<Task> CREATOR = new Parcelable.Creator<Task>() {
+        /**
+         * Create a new instance of the Task with the data previously
+         * written by writeToParcel().
+         *
+         * @param in The Parcel to read the object's data from
+         * @return New instance of Task
+         */
+        public Task createFromParcel(Parcel in) {
+            return new Task(in);
+        }
+
+        /**
+         * Create a new array of Task
+         * @param size Size of the array
+         * @return An array of Task
+         */
+        public Task[] newArray(int size) {
+            return new Task[size];
+        }
+    };
     private String name;
     private String description;
+
 
     /**
      * Constructor of the class
      *
-     * @param name        task's name
-     * @param description task's description
+     * @param name        Task's name
+     * @param description Task's description
      * @throws IllegalArgumentException if the parameter is null
      */
     public Task(String name, String description) {
@@ -26,16 +51,32 @@ public class Task implements Parcelable {
         }
     }
 
-    /* Order must be kept between the constructor and writeToParcel */
+    /**
+     * Private constructor used to recreate a Task when
+     * it was put inside an Intent.
+     *
+     * @param in Container of a Task
+     * @throws IllegalArgumentException if the parameter is null
+     */
     private Task(Parcel in) {
+        if (in == null) {
+            throw new IllegalArgumentException();
+        }
         this.name = in.readString();
         this.description = in.readString();
     }
 
     /**
+     * Getter returning a copy of the task's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
      * Setter to modify the task's name
      *
-     * @param newName the new task's name
+     * @param newName The new task's name
      * @throws IllegalArgumentException if newName is null
      */
     public void setName(String newName) {
@@ -47,9 +88,16 @@ public class Task implements Parcelable {
     }
 
     /**
+     * Getter returning a copy of the task's description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
      * Setter to modify the task's description
      *
-     * @param newDescription the new task's description
+     * @param newDescription The new task's description
      * @throws IllegalArgumentException if newName is null
      */
     public void setDescription(String newDescription) {
@@ -61,38 +109,25 @@ public class Task implements Parcelable {
     }
 
     /**
-     * Getter returning a copy of the task's name
+     * Override the describeContents method from
+     * Parcelable interface.
+     *
+     * @return 0
      */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Getter returning a copy of the task's description
-     */
-    public String getDescription() {
-        return description;
-    }
-
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Flatten the Task in to a Parcel
+     *
+     * @param dest  The parcel in which the Task should be written
+     * @param flags Flags about how the object should be written
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
         dest.writeString(description);
     }
-
-    // this is used to regenerate your object. All Parcelables must have a CREATOR that implements these two methods
-    public static final Parcelable.Creator<Task> CREATOR = new Parcelable.Creator<Task>() {
-        public Task createFromParcel(Parcel in) {
-            return new Task(in);
-        }
-
-        public Task[] newArray(int size) {
-            return new Task[size];
-        }
-    };
 }
