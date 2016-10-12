aa03e689b1de6b94f30df498bb096514de86acd9
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-12 15:09:15 +0200

    Creation of new test case
    To test the persistence of the database (i.e. when the user creates a task, then close the app, and then open it again, the task created is still displayed)

---
+++ app/src/androidTest/java/ch/epfl/sweng/project/DatabaseTest.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.project;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by cedric on 12.10.16.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+
+
+}
