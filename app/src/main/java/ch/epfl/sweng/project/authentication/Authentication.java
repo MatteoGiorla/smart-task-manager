3c533dd2aa7b993862712463ad756d0c09c2d7da
bnanchen <nanchenbastian@gmail.com>
2016-10-14 17:33:53 +0200

    #10 Avancements dans le user login
    Ajout des méthodes onCreate(), onStart(), onStop() et
    createUserWithEmailAndPassword()

--- app/src/main/java/ch/epfl/sweng/project/authentication/Authentication.java
+++ app/src/main/java/ch/epfl/sweng/project/authentication/Authentication.java
@@ -1,18 +1,75 @@
 package ch.epfl.sweng.project.authentication;
 
+import android.accounts.AccountManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.*;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.google.android.gms.internal.zzs.TAG;
 
 
 /**
  *
  */
 public class Authentication {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:"+ user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+            if (!task.isSuccessful()) {
+                Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 
 
 
