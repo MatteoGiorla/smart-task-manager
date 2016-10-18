bb81ffc611187c5956ff68e265fa70eac66045f5
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-18 23:24:25 +0200

    Added access check and User param
    Added method hasAccess to determine if the phone can access the remote storage device, as well as a User class parameter to retrieveAllData to know the credential to get the correct datas

--- app/src/main/java/ch/epfl/sweng/project/dataExchanger.java
+++ app/src/main/java/ch/epfl/sweng/project/dataExchanger.java
@@ -1,16 +1,28 @@
 package ch.epfl.sweng.project;
 
+import ch.epfl.sweng.project.authentication.User;
 
 public interface dataExchanger {
+
+    /**
+     * Checks if the remote storage device can be accessed at the
+     * moment.
+     *
+     * @return true if the remote storage device can be accessed,
+     *          false otherwise
+     */
+    public boolean hasAccess();
     /**
      * Take care of retrieving all user data if there is no
      * data locally stored on the app.
      *
+     * @param user the user credentials to retrieve its attached
+     *             datas from the remote storage device.
      * @return true if all data have been correctly retrieved,
      *          false if for some reason the action could not
      *          be performed
      */
-    public boolean retrieveAllData();
+    public boolean retrieveAllData(User user);
 
     /**
      *  Add a tasks to the remote storage device
