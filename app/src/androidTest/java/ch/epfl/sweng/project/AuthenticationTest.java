665ba7bb5e9c44a7cecd26b27bc1a225add94351
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-20 23:00:53 +0200

    Added facebook authentication test

--- app/src/androidTest/java/ch/epfl/sweng/project/AuthenticationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/AuthenticationTest.java
@@ -29,11 +29,14 @@ import static junit.framework.Assert.fail;
 public class AuthenticationTest {
     private String mGoogEmail;
     private String mGoogPassword;
+    private String mFacebookEmail;
+    private String mFacebookPassword;
     private UiDevice mUiDevice;
     private final static String NEXT_BUTTON_ID = "identifierNext";
     private final static String PASSWORD_NEXT_ID = "passwordNext";
     private final static String ACCEPT_ID = "next";
-    private final static String GOOGSERV_NEXT = "com.google.android.gms:id/suw_navbar_next";
+    private final static String FB_MAIL_ID = "com.facebook.katana:id/a4j";
+    private final static String FB_PASSWORD_ID = "com.facebook.katana:id/a4k";
     private long untilTimeout;
 
     @Before
@@ -41,6 +44,8 @@ public class AuthenticationTest {
         mUiDevice = getInstance(getInstrumentation());
         mGoogEmail = "trixyfinger@gmail.com";
         mGoogPassword = "sweng1234TaskIt";
+        mFacebookEmail = "cirdec3961@gmail.com";
+        mFacebookPassword = "Kristel";
         untilTimeout = 3000; //an estimation, but the connection procedure should not last more
     }
 
@@ -51,7 +56,6 @@ public class AuthenticationTest {
 
     private void removeAccount() {
         mUiDevice.pressHome();
-
     }
 
     @Rule
@@ -64,27 +68,69 @@ public class AuthenticationTest {
      */
     @Test
     public void GoogleLoginWorks() {
-            onView(withId(R.id.google_sign_in_button)).perform(click());
-            //first check if the user is already registered, if so just proceed to login.
-            UiObject mEmailText = mUiDevice.findObject(new UiSelector().text(mGoogEmail));
+        onView(withId(R.id.google_sign_in_button)).perform(click());
+        //first check if the user is already registered, if so just proceed to login.
+        UiObject mEmailText = mUiDevice.findObject(new UiSelector().text(mGoogEmail));
+        try{
+            mEmailText.click();
+            checkIfMainActivity();
+        } catch(UiObjectNotFoundException u1){
+            //if not, check if we are on the popup with
+            UiObject addAcount = mUiDevice.findObject(new UiSelector().text("Add account"));
             try{
-                mEmailText.click();
-                checkIfMainActivity();
-            } catch(UiObjectNotFoundException u1){
-                //if not, check if we are on the popup with
-                UiObject addAcount = mUiDevice.findObject(new UiSelector().text("Add account"));
-                try{
-                    addAcount.click();
-                    associateNewGoogleAccount();
-                }catch(UiObjectNotFoundException u2){
-                    associateNewGoogleAccount();
-                }
+                addAcount.click();
+                associateNewGoogleAccount();
+            }catch(UiObjectNotFoundException u2){
+                associateNewGoogleAccount();
             }
+        }
+    }
+
+    /**
+     * perform user like actions on the phone to authenticate
+     * oneself.
+     */
+    @Test
+    public void FacebookLoginWorks() {
+        onView(withId(R.id.facebook_sign_in_button)).perform(click());
+        //first check if the user is already registered, if so just proceed to login.
+        UiObject mEmailText = mUiDevice.findObject(new UiSelector().resourceId(FB_MAIL_ID));
+        try{
+            //removing the pre existent text if there is one.
+            String text = mEmailText.getText();
+            mEmailText.click();
+            mEmailText.click();
+            for(int i = 0; i<text.length(); ++i){
+                mUiDevice.pressDelete();
+            }
+
+            mEmailText.setText(mFacebookEmail);
+            UiObject mPasswordText = mUiDevice.findObject(new UiSelector().resourceId(FB_PASSWORD_ID));
+            try{
+                mPasswordText.click();
+                mPasswordText.setText(mFacebookPassword);
+
+                UiObject mLoginButton = mUiDevice.findObject(new UiSelector().text("LOG IN"));
+                mLoginButton.click();
+
+                checkIfMainActivity();
+            }catch(UiObjectNotFoundException u0){
+                fail("Error encountered while logging on facebook");
+            }
+            checkIfMainActivity();
+        } catch(UiObjectNotFoundException u1){
+            //if not, check if we reach the main activity
+            checkIfMainActivity();
+        }
     }
 
     /**
      * method that proceeds to go through all the authentification process
      * when it is the first time we try to connect a google account with the app.
+     *
+     * The test might takes some time after clicking on the sign in button,
+     * the transition between espresso and uiautomator might be responsible
+     * for that.
      */
     private void associateNewGoogleAccount() {
         try{
