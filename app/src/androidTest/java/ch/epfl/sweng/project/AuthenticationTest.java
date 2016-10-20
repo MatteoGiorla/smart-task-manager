bb34c57a7785b9791c48fa8f0b4927a5566d368f
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-20 22:09:29 +0200

    Finished coding google authentication test

--- app/src/androidTest/java/ch/epfl/sweng/project/AuthenticationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/AuthenticationTest.java
@@ -1,14 +1,14 @@
 package ch.epfl.sweng.project;
 
-
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,38 +16,57 @@ import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.project.authentication.LoginActivity;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.uiautomator.UiDevice.getInstance;
-import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class AuthenticationTest {
-    private String mEmail;
-    private String mPassword;
+    private String mGoogEmail;
+    private String mGoogPassword;
     private UiDevice mUiDevice;
-    private final static String NEXT_BUTTON = "NEXT";
-    private final static String ACCEPT_BUTTON = "ACCEPT";
+    private final static String NEXT_BUTTON_ID = "identifierNext";
+    private final static String PASSWORD_NEXT_ID = "passwordNext";
+    private final static String ACCEPT_ID = "next";
+    private final static String GOOGSERV_NEXT = "com.google.android.gms:id/suw_navbar_next";
     private long untilTimeout;
 
     @Before
     public void setup(){
-        mUiDevice = getInstance(InstrumentationRegistry.getInstrumentation());
-        mEmail = "trixyfinger@gmail.com";
-        mPassword = "sweng1234TaskIt";
-        untilTimeout = 5000;
+        mUiDevice = getInstance(getInstrumentation());
+        mGoogEmail = "trixyfinger@gmail.com";
+        mGoogPassword = "sweng1234TaskIt";
+        untilTimeout = 3000; //an estimation, but the connection procedure should not last more
+    }
+
+    @After
+    public void tearDown(){
+        removeAccount();
+    }
+
+    private void removeAccount() {
+        mUiDevice.pressHome();
+
     }
 
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    public ActivityTestRule<LoginActivity> mActivityRule =
+            new ActivityTestRule<LoginActivity>(LoginActivity.class);
 
+    /**
+     * perform user like actions on the phone to authenticate
+     * oneself.
+     */
     @Test
     public void GoogleLoginWorks() {
             onView(withId(R.id.google_sign_in_button)).perform(click());
             //first check if the user is already registered, if so just proceed to login.
-            UiObject mEmailText = mUiDevice.findObject(new UiSelector().text(mEmail));
+            UiObject mEmailText = mUiDevice.findObject(new UiSelector().text(mGoogEmail));
             try{
                 mEmailText.click();
                 checkIfMainActivity();
@@ -63,42 +82,44 @@ public class AuthenticationTest {
             }
     }
 
-    /* the google authentification with google should follow those steps:
-        type the test email under the "Enter your email" textview
-        then click "NEXT"
-        then enter the paswword under the "Password" textview
-        then click "NEXT"
-        then click "ACCEPT" "under the terms of service"
-        there is a wait (while loop until see NEXT) Then click NEXT
-        And the magic should have happened
+    /**
+     * method that proceeds to go through all the authentification process
+     * when it is the first time we try to connect a google account with the app.
      */
     private void associateNewGoogleAccount() {
         try{
             UiObject emailHint = mUiDevice.findObject(new UiSelector().text("Enter your email"));
-            emailHint.setText(mEmail);
-            UiObject nextAction = mUiDevice.findObject(new UiSelector().text(NEXT_BUTTON));
-            nextAction.click();
+            emailHint.setText(mGoogEmail);
+
+            UiObject nextAction = mUiDevice.findObject(new UiSelector().resourceId(NEXT_BUTTON_ID));
+            nextAction.clickAndWaitForNewWindow(untilTimeout);
+
             UiObject passwordHint = mUiDevice.findObject(new UiSelector().text("Password"));
-            passwordHint.setText(mPassword);
-            nextAction = mUiDevice.findObject(new UiSelector().text(NEXT_BUTTON));
-            nextAction.click();
-            UiObject acceptAction = mUiDevice.findObject(new UiSelector().text(ACCEPT_BUTTON));
-            mUiDevice.wait(untilTimeout);
-            nextAction = mUiDevice.findObject(new UiSelector().text(NEXT_BUTTON));
-            nextAction.click();
-            checkIfMainActivity();
+            passwordHint.setText(mGoogPassword);
+
+            nextAction = mUiDevice.findObject(new UiSelector().resourceId(PASSWORD_NEXT_ID));
+            nextAction.clickAndWaitForNewWindow(untilTimeout);
+
+            UiObject acceptAction = mUiDevice.findObject(new UiSelector().resourceId(ACCEPT_ID));
+            acceptAction.clickAndWaitForNewWindow();
+
+            UiScrollable googServices = new UiScrollable(new UiSelector().scrollable(true));
+            googServices.scrollForward();
+
+            UiObject nextGoogleServ = mUiDevice.findObject(new UiSelector().text("Next"));
+            nextGoogleServ.clickAndWaitForNewWindow();
+
         }catch (UiObjectNotFoundException u ){
             fail("There was an error while trying to associate new Google account with the app.");
-        }catch (InterruptedException i){
-            fail("There was an interruption during a wait during the authentification to Google, " +
-                    "try again or check out your network connection");
         }
+        checkIfMainActivity();
     }
 
+    /**
+     * serves as only checking if the add button is displayed,
+     * which is equivalent as checking if we reached mainActivity
+     */
     private void checkIfMainActivity(){
-        assertTrue(true);
+        onView(withId(R.id.add_task_button)).check(matches(isDisplayed()));
     }
-
-
  }
-
