9451ffafd8d69293cbe83a6c8071f905159d54fd
matteogiorla <matteo.giorla@me.com>
2016-10-25 19:06:45 +0200

    #20 Hiding keyboard when clicking outside edit text and design improvements

--- app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
@@ -1,6 +1,8 @@
 package ch.epfl.sweng.project;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
@@ -8,7 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -170,6 +174,28 @@ public abstract class TaskActivity extends AppCompatActivity {
         public void onClick(View v) {
             finish();
         }
+    }
+
+    /*
+     * Method to hide keyboard when clicking outside the EditTextView
+     *
+     * source : http://stackoverflow.com/questions/4828636/edittext-clear-focus-on-touch-outside
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 
     public void showDatePickerDialog(View v) {
