f7deee24a1644697d233bc1b9318c7fe6cd7732c
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-10-25 16:54:08 +0200

    #29 User class is now compatible with firebase

--- app/src/main/java/ch/epfl/sweng/project/User.java
+++ app/src/main/java/ch/epfl/sweng/project/User.java
@@ -1,11 +1,5 @@
 package ch.epfl.sweng.project;
 
-import android.util.Log;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.FirebaseUser;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,40 +9,37 @@ import java.util.List;
 public class User {
     private String email;
     private List listTasks;
-    private Location currentLocation;
-    private Task.Energy currentEnergy;
-    private long currentTimeAtDisposal;
     private List<Location> listLocations;
     private static final String TAG = "User Class";
 
     /**
-     * Constructor of the class. Implementation of the fields of the class with
-     * default values.
+     * Constructor of the class with only email. The list of task's id and location
+     * are initialized with empty arrays.
      *
-     * @throws FirebaseAuthInvalidUserException if the FirebaseUser user doesn't exist.
+     * @param mail The email of the user
      */
-    public User() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            this.email = user.getEmail();
+    public User(String mail) {
+        if (mail != null) {
+            this.email = mail;
         } else {
-            Log.d(TAG, "Error the user doesn't exist.");
-            throw new NullPointerException();
+            throw new NullPointerException("User email is invalid !");
         }
-        // Default values:
-        this.listTasks = new ArrayList<Long>();
-        this.currentLocation = new Location("", Location.LocationType.EVERYWHERE, null);
-        this.currentEnergy = Task.Energy.NORMAL;
-        this.currentTimeAtDisposal = 30;
-        Location home = new Location("Home", Location.LocationType.HOME, null);
-        Location office = new Location("Office", Location.LocationType.WORKPLACE, null);
-        Location school = new Location("School", Location.LocationType.WORKPLACE, null);
-        Location everywhere = new Location("", Location.LocationType.EVERYWHERE, null);
+        this.listTasks = new ArrayList<String>();
         this.listLocations = new ArrayList<Location>();
-        this.listLocations.add(home);
-        this.listLocations.add(office);
-        this.listLocations.add(school);
-        this.listLocations.add(everywhere);
+    }
+    /**
+     * Constructor of the class.
+     *
+     * @throws NullPointerException if the parameters are invalid
+     */
+    public User(String mail, List<String> listTasks, List<Location> locations) {
+        if (mail == null || listTasks == null || locations == null) {
+            throw new NullPointerException("Invalid parameters when creating a new user");
+        } else {
+            this.email = mail;
+            this.listTasks = new ArrayList<String>(listTasks);
+            this.listLocations = new ArrayList<Location>(locations);
+        }
     }
 
     /**
@@ -65,7 +56,7 @@ public class User {
      *
      * @return the list of Tasks of the user.
      */
-    public List<Long> getListTasks() {
+    public List<String> getListTasks() {
         return this.listTasks;
     }
 
@@ -74,7 +65,7 @@ public class User {
      *
      * @param idTask id of the task to add.
      */
-    public void addListTasks(long idTask) {
+    public void addListTasks(String idTask) {
         this.listTasks.add(idTask);
     }
 
@@ -85,62 +76,9 @@ public class User {
      * @return true if the id of the task was inside the list and was well removed, otherwise false.
      */
     public boolean removeListTasks(long idTask) {
-            return this.listTasks.remove(idTask);
+        return this.listTasks.remove(idTask);
     }
 
-    /**
-     * Getter
-     *
-     * @return the current location of the user.
-     */
-    public Location getCurrentLocation() {
-        return this.currentLocation;
-    }
-
-    /**
-     * Setter
-     *
-     * @param newLocation the new current location of the user.
-     */
-    public void setCurrentLocation(Location newLocation) {
-        this.currentLocation = newLocation;
-    }
-
-    /**
-     * Getter
-     *
-     * @return the current energy of the user.
-     */
-    public Task.Energy getCurrentEnergy() {
-        return this.currentEnergy;
-    }
-
-    /**
-     * Setter
-     *
-     * @param newEnergy the new current energy of the user.
-     */
-    public void setCurrentEnergy(Task.Energy newEnergy) {
-        this.currentEnergy = newEnergy;
-    }
-
-    /**
-     * Getter
-     *
-     * @return the current time at disposal of the user.
-     */
-    public long getCurrentTimeAtDisposal() {
-        return this.currentTimeAtDisposal;
-    }
-
-    /**
-     * Setter
-     *
-     * @param newTimeAtDisposal the new current time at disposal of the user.
-     */
-    public void setCurrentTimeAtDisposal(long newTimeAtDisposal) {
-        this.currentTimeAtDisposal = newTimeAtDisposal;
-    }
 
     /**
      * Add a new location to the list of the locations of the user.
