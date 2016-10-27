d891a5bddee3fe2d93df275c04b9e840a8e86336
matteogiorla <matteo.giorla@me.com>
2016-10-27 13:19:04 +0200

    #20 Clean up - Optimized imports

--- app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
@@ -15,7 +15,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
