50feac9445dd96f726d95fb89045d5e6ebd60659
MikaelMorales <mikael.moralesgonzalez@epfl.ch>
2016-10-31 20:30:18 +0100

    Trying to fix parsing error

--- app/src/main/java/ch/epfl/sweng/project/data/DataProvider.java
+++ app/src/main/java/ch/epfl/sweng/project/data/DataProvider.java
@@ -15,25 +15,25 @@ public class DataProvider {
     public static final String TEST_PROVIDER = "Tests";
 
     private static String mProvider = FIREBASE_PROVIDER;
-    private final DatabaseReference mDatabase;
     private final TaskListAdapter mAdapter;
     private final ArrayList<Task> mTaskList;
     private final Context mContext;
 
     public DataProvider(Context context, TaskListAdapter adapter, ArrayList<Task> taskList) {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         mAdapter = adapter;
         mTaskList = taskList;
         mContext = context;
     }
 
     public DataExchanger getProvider() {
-        if(mProvider.equals(FIREBASE_PROVIDER)) {
-            return new FirebaseDataExchanger(mContext,mAdapter,mTaskList);
-        }else if(mProvider.equals(TEST_PROVIDER)) {
-            return new LocalDataExchanger(mAdapter, mTaskList);
-        }else{
-            throw new IllegalArgumentException("This provider does not exists !");
+        switch (mProvider) {
+            case FIREBASE_PROVIDER:
+                return new FirebaseDataExchanger(mContext, mAdapter, mTaskList);
+            case TEST_PROVIDER:
+                return new LocalDataExchanger(mAdapter, mTaskList);
+            default:
+                throw new IllegalArgumentException("This provider does not exists !");
         }
     }
 
