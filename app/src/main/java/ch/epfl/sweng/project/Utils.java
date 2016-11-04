0808144fdae27010c2922a2bbde239da2e6db394
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-11-04 13:02:57 +0100

    Added user manipulation in Firebase functions

--- app/src/main/java/ch/epfl/sweng/project/Utils.java
+++ app/src/main/java/ch/epfl/sweng/project/Utils.java
@@ -1,6 +1,9 @@
 package ch.epfl.sweng.project;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Utils {
 
     /**
@@ -11,5 +14,28 @@ public class Utils {
      */
     public static String encodeMailAsFirebaseKey(String mail) {
         return mail.replace('.', ' ');
+    }
+
+
+    public static void addUser(User user) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userRef = mDatabase.child("users").child(Utils.encodeMailAsFirebaseKey(user.getEmail())).getRef();
+        userRef.setValue(user);
+    }
+
+    public static void updateUser(User user) {
+        deleteUser(user);
+        addUser(user);
+    }
+
+    /**
+     * Deleter a user in the database
+     *
+     * @param user The user to be deleted
+     */
+    private static void deleteUser(User user) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userRef = mDatabase.child("users").child(Utils.encodeMailAsFirebaseKey(user.getEmail())).getRef();
+        userRef.removeValue();
     }
 }
