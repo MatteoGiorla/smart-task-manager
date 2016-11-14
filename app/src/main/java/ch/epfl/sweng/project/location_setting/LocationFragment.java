9bb6b1e9320df75cf9ccbbe16a4f4c143d1ce107
bnanchen <nanchenbastian@gmail.com>
2016-11-14 09:06:58 +0100

    Addition of the floating_location_menu inside LocationFragment.java

--- app/src/main/java/ch/epfl/sweng/project/location_setting/LocationFragment.java
+++ app/src/main/java/ch/epfl/sweng/project/location_setting/LocationFragment.java
@@ -191,7 +191,7 @@ public class LocationFragment extends Fragment {
 
         //if(selectedId != 0 && selectedId != 1) { //prevent default locations from edit or delete
             MenuInflater menuInflater = getActivity().getMenuInflater();
-            menuInflater.inflate(R.menu.floating_context_menu, menu);
+            menuInflater.inflate(R.menu.floating_location_menu, menu);
         //}
 
     }
