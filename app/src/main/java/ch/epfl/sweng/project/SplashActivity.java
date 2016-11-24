1f5affa8963de4cf13b17a97ed9dffd80127d721
matteogiorla <matteo.giorla@me.com>
2016-11-24 10:56:15 +0100

    Added splash screen on app opening

---
+++ app/src/main/java/ch/epfl/sweng/project/SplashActivity.java
@@ -0,0 +1,17 @@
+package ch.epfl.sweng.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, EntryActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
