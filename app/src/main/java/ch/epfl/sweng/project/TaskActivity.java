28e23821b30e4021c0399759730446d6c9e62272
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2016-12-02 17:44:27 +0100

    delete unused error icon

--- app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/TaskActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -168,12 +167,10 @@ public abstract class TaskActivity extends AppCompatActivity {
         public void afterTextChanged(Editable s) {
             if (titleIsNotUnique(s.toString())) {
                 doneEditButton.setVisibility(View.INVISIBLE);
-                titleEditText.setError(getResources().getText(R.string.error_title_duplicated),
-                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_18dp));
+                titleEditText.setError(getResources().getText(R.string.error_title_duplicated));
             } else if (s.toString().isEmpty()) {
                 doneEditButton.setVisibility(View.INVISIBLE);
-                titleEditText.setError(getResources().getText(R.string.error_title_empty),
-                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_18dp));
+                titleEditText.setError(getResources().getText(R.string.error_title_empty));
             } else {
                 doneEditButton.setVisibility(View.VISIBLE);
             }
