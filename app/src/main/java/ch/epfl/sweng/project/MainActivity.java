1565a927662613756baf120e30ac06ca17d381db
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2016-12-02 18:49:40 +0100

    correct typo and optimize import

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
