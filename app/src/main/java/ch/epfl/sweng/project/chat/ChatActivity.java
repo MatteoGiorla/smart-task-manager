8e909d47c0649a37ea04a486a17ea011242bf9d8
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2016-12-05 15:27:01 +0100

    Complete the ChatActivity
    implement Listener, when clicking on the send button, create a Message instance with the message typed by the user

--- app/src/main/java/ch/epfl/sweng/project/chat/ChatActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/chat/ChatActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Date;
 
 import ch.epfl.sweng.project.R;
 import ch.epfl.sweng.project.Task;
@@ -13,7 +21,6 @@ public class ChatActivity extends AppCompatActivity {
 
     private Intent intent;
     private Task task;
-    private MessageAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,13 +30,23 @@ public class ChatActivity extends AppCompatActivity {
         //Initialise the Task and check its validity
         getAndCheckIntent();
 
-        //Initialise the MessageAdapter
-        mAdapter = new MessageAdapter(this, R.layout.list_item_chat, task.getListOfMessages());
+        setAdapter();
 
+        Button sendMssgButton = (Button) findViewById(R.id.send_message_button);
+        sendMssgButton.setOnClickListener(new SendMessageOnClickListener());
     }
 
+    private void setAdapter() {
+        //Initialise the MessageAdapter
+        MessageAdapter mAdapter = new MessageAdapter(this,
+                R.layout.list_item_chat,
+                task.getListOfMessages());
 
-//    private void populate
+        //Get the listView
+        ListView mssgListView = (ListView) findViewById(R.id.list_of_messages);
+        //Bind the adapter to the listView
+        mssgListView.setAdapter(mAdapter);
+    }
 
     private void getAndCheckIntent() {
         intent = getIntent();
@@ -43,6 +60,29 @@ public class ChatActivity extends AppCompatActivity {
         task = intent.getParcelableExtra(TASK_CHAT_KEY);
         if(task == null) {
             throw new IllegalArgumentException("Task passed with the intent to ChatActivity is null");
+        }
+    }
+
+    private class SendMessageOnClickListener implements View.OnClickListener {
+
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
+        @Override
+        public void onClick(View v) {
+            EditText editMssg = (EditText) findViewById(R.id.input);
+            if(editMssg.getText() != null) {
+                String mssgText = editMssg.getText().toString();
+
+                if(!mssgText.isEmpty()) {
+                    String userName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+                    long time = new Date().getTime();
+                    Message newMessage = new Message(userName, mssgText, time);
+                    task.addMessage(newMessage);
+                }
+            }
         }
     }
 }
