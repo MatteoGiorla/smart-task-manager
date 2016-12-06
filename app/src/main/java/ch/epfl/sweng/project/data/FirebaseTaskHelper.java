131f6c08414a2f25dee0748647ff3ea247e3b563
Cédric Viaccoz <cedric.viaccoz@gmail.com>
2016-12-06 22:05:30 +0100

    #121 Refactored update and delete for tasks with multiple contributors

--- app/src/main/java/ch/epfl/sweng/project/data/FirebaseTaskHelper.java
+++ app/src/main/java/ch/epfl/sweng/project/data/FirebaseTaskHelper.java
@@ -77,49 +77,110 @@ public class FirebaseTaskHelper implements TaskHelper {
 
     @Override
     public void addNewTask(Task task, int position) {
+        Task toAdd = task;
         if(Utils.hasContributors(task)){
             //in this case, it is the duty of newTask/editTaskActivity
             //to give a title with the correct format (title@@email@@email
             for (String mail : task.getListOfContributors()) {
-                String[] title = Utils.separateTitleAndSuffix(task.getName());
-                String[] suffix = Utils.getCreatorAndSharer(title[1]);
-                Task toAdd;
-                if(suffix[1].equals(mail)){
-                    //in the case where we add the task to the creator, nothing to preprocess.
-                    toAdd = task;
-                }else{
-                    String newTitle = Utils.constructSharedTitle(title[0],suffix[0],mail);
-                    String locationName = Utils.getEverywhereLocation();
-                    toAdd = new Task(newTitle,task.getDescription(),locationName,task.getDueDate(),task.getDuration(),task.getEnergy().toString(),task.getListOfContributors());
-                }
-                DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(task.getName()).getRef();
+                toAdd = sharedTaskPreProcessing(task, mail, Utils.getEverywhereLocation());
+                DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(toAdd.getName()).getRef();
                 taskRef.setValue(toAdd);
             }
         }else{
             DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(task.getListOfContributors().get(0))).child(task.getName()).getRef();
-            taskRef.setValue(task);
+            taskRef.setValue(toAdd);
         }
-        mAdapter.add(task, position);
+        mAdapter.add(toAdd, position);
+    }
+
+    /**
+     * Takes care of preparing a shared task given the email of the shared user, its personal choosen
+     * location and the original task.
+     *
+     * @param task the task to process
+     * @param mail the mail of the person shared
+     * @param locationName the custom location of the person.
+     * @return a newly created task having the correct name and location according to the name.
+     */
+    public Task sharedTaskPreProcessing(Task task, String mail, String locationName){
+        String[] title = Utils.separateTitleAndSuffix(task.getName());
+        String[] suffix = Utils.getCreatorAndSharer(title[1]);
+        Task toAdd;
+        if(suffix[1].equals(mail)){
+            //in the case where we add the task to the creator, nothing to preprocess.
+            toAdd = task;
+        }else{
+            String newTitle = Utils.constructSharedTitle(title[0],suffix[0],mail);
+            toAdd = new Task(newTitle,task.getDescription(),locationName,task.getDueDate(),task.getDuration(),task.getEnergy().toString(),task.getListOfContributors());
+        }
+        return toAdd;
     }
 
     @Override
     public void deleteTask(Task task, int position) {
-        for (String mail : task.getListOfContributors()) {
-            DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(task.getName()).getRef();
+        if(Utils.hasContributors(task)){
+            for (String mail : task.getListOfContributors()) {
+                String taskTitle = sharedTaskPreProcessing(task, mail, Utils.getEverywhereLocation()).getName();
+                DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(taskTitle).getRef();
+                taskRef.removeValue();
+            }
+        }else{
+            DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(task.getListOfContributors().get(0))).child(task.getName()).getRef();
             taskRef.removeValue();
         }
         mAdapter.remove(position);
     }
 
     @Override
-    public void updateTask(Task original, Task updated, int position) {
-        deleteTask(original, position);
-        addNewTask(updated, position);
+    public void updateTask(final Task original, final Task updated, int position) {
+        if(Utils.hasContributors(original)){
+            for(final String mail : updated.getListOfContributors()){
+                String locationName;
+                if(original.getListOfContributors().contains(mail)){
+
+                    //This is risky, might not work
+                    DatabaseReference locationRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(original.getName()).child("locationName");
+                    locationRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String locationName = (String) dataSnapshot.getValue();
+                            String oldName =  sharedTaskPreProcessing(original, mail, locationName).getName();
+
+                            Task updatedTask = sharedTaskPreProcessing(updated, mail, locationName);
+                            //removing the old task
+                            DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(oldName).getRef();
+                            taskRef.removeValue();
+
+                            //adding the new task.
+                            DatabaseReference taskReference = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(updated.getName()).getRef();
+                            taskReference.setValue(updatedTask);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {}
+                    });
+                }else{
+                    locationName = Utils.getEverywhereLocation();
+                    Task updatedTask = sharedTaskPreProcessing(updated, mail, locationName);
+                    DatabaseReference taskRef = mDatabase.child("tasks").child(Utils.encodeMailAsFirebaseKey(mail)).child(updated.getName()).getRef();
+                    taskRef.setValue(updatedTask);
+                }
+            }
+
+            mAdapter.remove(position);
+            mAdapter.add(updated, position);
+
+        }else{
+            //in either the case of no contributors, or no contributors previously but contributors
+            //now, we can simply delete then add the task.
+            deleteTask(original, position);
+            addNewTask(updated, position);
+        }
     }
 
 
     /**
-     * Reconstruct the tasks form the DataSnapshot given.
+     * Reconstruct the tasks from the DataSnapshot given.
      *
      * @param dataSnapshot Data recovered from the database
      */
