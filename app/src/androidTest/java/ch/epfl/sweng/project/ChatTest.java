762290beb43c3b073c1105cd1e1ef423f45b5f0c
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2016-12-08 15:27:45 +0100

    implement first test for ChatActivity
    Test that when we send a message, it's displayed in the listView

--- app/src/androidTest/java/ch/epfl/sweng/project/ChatTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/ChatTest.java
@@ -8,7 +8,11 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class ChatTest extends SuperTest {
 
@@ -29,5 +33,8 @@ public class ChatTest extends SuperTest {
     @Test
     public void messageSendIsDisplayed() {
         openChat();
+        onView(withId(R.id.input)).perform(typeText("Hello"));
+        onView(withId(R.id.send_message_button)).perform(click());
+        onView(withId(R.id.list_of_messages)).check(matches(hasDescendant(withText("Hello"))));
     }
 }
