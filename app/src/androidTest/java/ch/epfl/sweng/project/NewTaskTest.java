77645f9e77edec65ad0b8e974c5e738270fe9967
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2016-12-08 16:25:50 +0100

    fix dueTaskDisplayDays test
    Checks if "today" is displayed in the listview if we are the first of the month
    Otherwise test that "late" is displayed

--- app/src/androidTest/java/ch/epfl/sweng/project/NewTaskTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/NewTaskTest.java
@@ -1,6 +1,9 @@
 package ch.epfl.sweng.project;
 
 import android.content.Context;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -8,6 +11,7 @@ import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -114,8 +118,12 @@ public final class NewTaskTest extends SuperTest {
         onView(withContentDescription("represent the remaining days")).check(matches(withText(containsString("days left"))));
     }
 
+    /**
+     * Test which indicator is displayed in listView (late or due today)
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Test
-    public void lateTaskDisplayDays(){
+    public void dueTaskDisplayDays(){
         UiDevice mUiDevice = getInstance(getInstrumentation());
 
         onView(withId(R.id.add_task_button)).perform(click());
@@ -124,7 +132,14 @@ public final class NewTaskTest extends SuperTest {
         onView(withId(R.id.title_task)).perform(typeText("task"));
         pressBack();
 
-        //add a due date !!! Warning, test only working launched after the first of every month
+        String dayToSelect = "1";
+        final Calendar c = Calendar.getInstance();
+        final int currentDay = c.get(Calendar.DAY_OF_MONTH);
+        Log.e("current day ", "current day =  " + currentDay);
+        if(currentDay == 1) {
+            dayToSelect = String.valueOf(currentDay - 1);
+        }
+
         onView(withId(R.id.pick_date)).perform(click());
         UiObject thirtyButton = mUiDevice.findObject(new UiSelector().text("1"));
         UiObject okButton = mUiDevice.findObject(new UiSelector().text("OK"));
@@ -141,9 +156,13 @@ public final class NewTaskTest extends SuperTest {
 
         onView(withId(R.id.edit_done_button_toolbar)).perform(click());
 
-        //to add "s" after day after the two of december
-        onView(withContentDescription("represent the remaining days"))
-                .check(matches(withText(containsString("day late"))));
+        if(currentDay == 1) {
+            onView(withContentDescription("represent the remaining days"))
+                    .check(matches(withText(containsString("today"))));
+        } else {
+            onView(withContentDescription("represent the remaining days"))
+                    .check(matches(withText(containsString("late"))));
+        }
     }
 
     @Test
